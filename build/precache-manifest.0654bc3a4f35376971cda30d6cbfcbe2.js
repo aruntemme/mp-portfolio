aeb4ec86b8e4ae5af7a46c199ccbdb4a33443310
Arun K <arungauthamk@gmail.com>
2020-09-09 14:28:01 +0530

    deployed 🔥

---
+++ build/precache-manifest.0654bc3a4f35376971cda30d6cbfcbe2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f36e2e67a29bf674983d28f329db791",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8fadb895bcde4fedefde",
+    "url": "/static/css/main.bfbcff86.chunk.css"
+  },
+  {
+    "revision": "99d0f0c9f490d651f6e3",
+    "url": "/static/js/2.19fdf293.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/static/js/2.19fdf293.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fadb895bcde4fedefde",
+    "url": "/static/js/main.73d4e971.chunk.js"
+  },
+  {
+    "revision": "cb3ccd3eb684c83ffcee",
+    "url": "/static/js/runtime-main.823c8cbe.js"
+  }
+]);
